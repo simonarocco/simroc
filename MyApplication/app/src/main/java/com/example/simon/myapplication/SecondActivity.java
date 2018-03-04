f60a04ef6bf12198c489ec17ceb3523956e9f0b4
simonarocco <37022160+simonarocco@users.noreply.github.com>
2018-03-03 22:14:25 -0500

    Backend of login page

--- MyApplication/app/src/main/java/com/example/simon/myapplication/SecondActivity.java
+++ MyApplication/app/src/main/java/com/example/simon/myapplication/SecondActivity.java
@@ -2,8 +2,17 @@ package com.example.simon.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private EditText Username;
+    private EditText Password;
+    private EditText Password2;
+    private Button Create;
+
+    Username =
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
